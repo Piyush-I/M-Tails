fea23dcf5909bb2bd60abc0331e984d87bb4c825
Piyush-I <piyush.ingle14@gmail.com>
2019-10-12 17:00:19 +0530

    First commit

--- app/src/main/java/com/example/android/internetspeedtest/MainActivity.java
+++ app/src/main/java/com/example/android/internetspeedtest/MainActivity.java
@@ -199,66 +199,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    /*
-    public class customCall extends AsyncTask<Void , Void , Void> {
-        SpeedTestSocket speedTestSocket = new SpeedTestSocket();
-        @Override
-        protected Void doInBackground(Void... voids) {
-            speedTestSocket.startFixedDownload("http://www.ovh.net/files/100Mio.dat", 10000);
-            //speedTestSocket.startUpload("http://ipv4.ikoula.testdebit.info/", 1000000);
-            return null;
-        }
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
-            speedTestSocket.addSpeedTestListener(new ISpeedTestListener() {
-
-                @Override
-                public void onCompletion(SpeedTestReport report) {
-                    // called when download/upload is complete
-
-
-                    Log.d("SPEED" , "[COMPLETED] rate in octet/s : " + report.getTransferRateOctet());
-                    Log.d("SPEED" , "[COMPLETED] rate in bit/s   : " + report.getTransferRateBit());
-                }
-
-                @Override
-                public void onError(SpeedTestError speedTestError, String errorMessage) {
-                    // called when a download/upload error occur
-                }
-
-                @Override
-                public void onProgress(float percent, SpeedTestReport report) {
-                    // called to notify download/upload progress
-                    System.out.println("[PROGRESS] progress : " + percent + "%");
-                    System.out.println("[PROGRESS] rate in octet/s : " + report.getTransferRateOctet());
-                    System.out.println("[PROGRESS] rate in bit/s   : " + report.getTransferRateBit());
-
-                    Log.d("SPEED" , "[PROGRESS] progress : " + percent + "%");
-                    Log.d("SPEED" , "[PROGRESS] rate in octet/s : " + report.getTransferRateOctet());
-                    Log.d("SPEED" , "[PROGRESS] rate in bit/s   : " + report.getTransferRateBit());
-
-                    @SuppressLint({"NewApi", "LocalSuppress"}) BigDecimal bd = new BigDecimal(report.getTransferRateBit());
-                    @SuppressLint({"NewApi", "LocalSuppress"}) long speed = bd.setScale(0, BigDecimal.ROUND_HALF_UP).longValue();
-                    final long finalSpeed = (speed/8192)/100;
-
-                    Log.d("METER_SPEED", ""+finalSpeed);
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            pointerSpeed.speedTo(finalSpeed);
-                        }
-                    });
-
-                }
-            });
-
-        }
-    }
-*/
 
 
 
